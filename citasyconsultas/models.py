d37ce9ee9f60d54a2812ac01725008b3b5929baf
FranklinPerez <franklin9perez952012@gmail.com>
2019-06-19 11:48:41 -0600

    modelos departamento, municipio, paciente

--- citasyconsultas/models.py
+++ citasyconsultas/models.py
@@ -86,3 +86,26 @@ class Consulta(models.Model):
 	servicios=models.ManyToManyField(Servicio)
 	def __str__(self):
 		return self.codCon
+
+class Departamento(models.Model):
+	numDep = models.IntegerField(primary_key = True)
+	nomDep = models.CharField(max_length = 100, help_text = "Ingrese un departamento")
+	def __str__(self):
+		return self.nomDep
+
+class Municipio(models.Model):
+	numMunicipio = models.IntegerField(primary_key = True)
+	nomMunicipio = models.CharField(max_length = 100, help_text = "Ingrese un municipio")
+	departamento = models.ForeignKey(Departamento, on_delete = models.CASCADE)
+	def __str__(self):
+		return self.nomMunicipio
+
+class Paciente(models.Model):
+	numExpediente = models.IntegerField(primary_key = True)
+	nomPaciente = models.CharField(max_length = 200, help_text = "Ingrese el nombre del paciente")
+	apelPaciente = models.CharField(max_length = 200, help_text = "Ingrese el apellido del paciente")
+	fechaNacimiento = models.DateField()
+	emailPaciente = models.CharField(max_length = 200, help_text = "Ingrese eMail del paciente")
+	telefono = models.CharField(max_length = 8, help_text = "Ingrese el telefono del paciente")
+	def __str__(self):
+		return self.nomPaciente
